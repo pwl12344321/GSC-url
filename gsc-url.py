12601c410c6e7b1fcc134d94201a4939776df926
Sam Pan <pwl12344321@gmail.com>
2022-08-11 01:34:14 +0800

    fill more lists

--- gsc-url.py
+++ gsc-url.py
@@ -5,25 +5,26 @@ import pandas as pd
 df = pd.read_excel('brand.xlsx')
 brands = list(df.Brandname) # brands is a list & Brandname is header
 categories = ['skincare', 'makeup', 'perfume', 'mens-skincare', 'cologne', 'haircare', 'health-foods']
-subC = 'groupid='
-subsubC = 'typeid='
+subCs = ['groupid=', 'groupId=']
+subsubC = 'typeId='
 sort = 'sort='
-
 
 # read and export url excel
 df = pd.read_excel('url.xlsx')
 
 def myfunc(x):
-	if any(brand in x for brand in brands) and any(category in x for category in categories):
+	if any(brand in x for brand in brands) and any(category in x for category in categories) and not any(subC in x for subC in subCs) and not subsubC in x and not sort in x:
 		return "brand under category"
-	elif any(brand in x for brand in brands) and any(category in x for category in categories) and subC in x:
+	elif any(brand in x for brand in brands) and any(category in x for category in categories) and any(subC in x for subC in subCs) and not subsubC in x and not sort in x:
 		return "brand under category w/ sub category"
-	elif any(brand in x for brand in brands) and any(category in x for category in categories) and subC in x and subsubC in x:
+	elif any(brand in x for brand in brands) and any(category in x for category in categories) and any(subC in x for subC in subCs) and subsubC in x and not sort in x:
 		return "brand under category w/ sub sub category"
+	elif sort in x:
+		return "with sort"
 	else:
 		return "other type"
 
 df['KindOfType'] = df['URL'].apply(myfunc) # URL is header
 print(df)
 
-df.to_excel('brandtest.xlsx')
+df.to_excel('urltest.xlsx')
